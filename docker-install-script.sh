303a5b9bfdb3bd7302738eb2c67033b73f49bc6a
Miguel-Marcano <miguel.marcano@uconn.edu>
2021-08-21 17:48:15 -0400

    dockers

---
+++ docker-install-script.sh
@@ -0,0 +1,20 @@
+echo "installing docker"
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+echo "install the repository"
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+echo "install the engine"
+sudo yum install docker-ce --nobest
+echo "starting docker"
+sudo systemctl start docker
+echo "check to see if docker is running"
+#sudo docker run hello-world
